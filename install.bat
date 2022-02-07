4deefa9022f86bede9cb08391c4f6e5cacc4bc55
Chill Nook <83742340+chillnook@users.noreply.github.com>
2022-02-06 23:21:41 -0500

    Delete install.bat

--- install.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-start https://www.python.org/ftp/python/3.10.2/python-3.10.2-amd64.exe
-pause
-pip install psutil
-pause
-echo Installation finshed. You may saftely close the window.
-pause

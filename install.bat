e3fdaf58434dc2a4b64348c353a66495f618dd10
Chill Nook <83742340+chillnook@users.noreply.github.com>
2022-02-07 01:43:15 -0500

    Committed files.

---
+++ install.bat
@@ -0,0 +1,15 @@
+@echo off
+start https://www.python.org/ftp/python/3.10.2/python-3.10.2-amd64.exe
+echo Press ENTER to continue the installation.
+pause
+pip install psutil
+echo Press ENTER to continue the installation.
+pause
+pip install pyttsx3
+echo Press ENTER to continue the installation.
+pause
+pip install keyboard
+echo Press ENTER to continue the installation.
+pause
+echo Installation finshed. You may saftely close the window.
+pause
